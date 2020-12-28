fb0c68c280bc170d0b549874579c8e63e8ca50e3
xuliang <17770048437@163.com>
2020-12-28 10:41:50 +0800

    1.0.1

--- clockIn/src/main/java/com/example/clockIn/source/MyMvcConfig.java
+++ clockIn/src/main/java/com/example/clockIn/source/MyMvcConfig.java
@@ -2,6 +2,7 @@ package com.example.clockIn.source;
 
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
@@ -11,5 +12,11 @@ public class MyMvcConfig extends WebMvcConfigurationSupport {
         registry.addViewController("/").setViewName("index");
 //        registry.addViewController("/login").setViewName("login");
 //        registry.addViewController("/index").setViewName("index");
+    }
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/")
+                .addResourceLocations("classpath:/templates/");
+        super.addResourceHandlers(registry);
     }
 }
